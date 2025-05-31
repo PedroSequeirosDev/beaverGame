05177f7fa717e3fa2ecc131771e0af7faaea1912
PedroSequeirosDev <pedro.sequeiros1997@hotmail.com>
2025-05-31 23:08:32 +0100

    add basic menu and fade script

---
+++ Assets/scripts/fade.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class fade : MonoBehaviour
+{
+    [SerializeField] public float fadeDuration = 2f;
+    private Image fader;
+
+    void Start()
+    {
+        fader = GetComponent<Image>();
+    }
+
+    public IEnumerator FadeIn()
+    {
+        fader.raycastTarget = true;
+        float timer = 0f;
+        Color color = fader.color;
+
+        while (timer < fadeDuration)
+        {
+            timer += Time.deltaTime;
+            color.a = Mathf.Lerp(0, 1, timer / fadeDuration);
+            fader.color = color;
+            yield return null;
+        }
+        color.a = 1;
+        fader.color = color;
+    }
+}
---
+++ Assets/scripts/menuScript.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class menuScript : MonoBehaviour
+{
+    public fade screenfade;
+    public void StartGame()
+    {
+        StartCoroutine(FadeNextScene());
+    }
+
+    private IEnumerator FadeNextScene()
+    {
+        yield return screenfade.FadeIn();
+        SceneManager.LoadScene("BeaverFever");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quitting game");
+        Application.Quit();
+    }
+}
